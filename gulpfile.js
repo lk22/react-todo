20b9da21c2cd49489e3eec527a729396fb20d3d3
Leo Knudsen <nightskater-220@hotmail.com>
2017-01-21 13:35:42 +0100

    make boilerplate gulp ready

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', ['sass', 'watch'], function() {
+	console.log('stylesheet compiled succefully');
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./resources/assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', ['sass'], function() {
+	gulp.watch('./resources/assets/sass/**/*.scss');
+});
