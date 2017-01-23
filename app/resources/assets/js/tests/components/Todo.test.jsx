7c023ac9b13d94da3eaed3e238ea5925d05225a7
Leo Knudsen <nightskater-220@hotmail.com>
2017-01-23 01:32:34 +0100

    test commit for heroku after renaming

--- app/resources/assets/js/tests/components/Todo.test.jsx
+++ app/resources/assets/js/tests/components/Todo.test.jsx
@@ -4,6 +4,7 @@ var TestUtils = require('react-addons-test-utils');
 var $ = require('jQuery');
 var expect = require('expect');
 
+var TodoList = require('TodoList');
 var Todo = require('Todo');
 
 describe('Todo, </Todo>', () =>
@@ -11,5 +12,24 @@ describe('Todo, </Todo>', () =>
 	it('should exist', () =>
 	{
 		expect(Todo).toExist();
+	});
+
+	it('should render on todo component for each todo item', () =>
+	{
+		var todos = [
+			{
+				id: 1,
+				text: 'something'
+			},
+			{
+				id: 2,
+				text: 'something else'
+			}
+		];
+
+		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+		var todoComponents = TestUtils.scryRenderedComponentsWithType(TodoList, Todo);
+
+		expect(todoComponents.length).toBe(todos.length);
 	});
 });
--- app/resources/assets/js/tests/components/TodoList.test.jsx
+++ app/resources/assets/js/tests/components/TodoList.test.jsx
@@ -5,11 +5,31 @@ var $ = require('jQuery');
 var expect = require('expect');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList, </TodoList>', () =>
 {
 	it('should exist', () =>
 	{
 		expect(TodoList).toExist();
+	});
+
+	it('should render on todo component for each todo item', () =>
+	{
+		var todos = [
+			{
+				id: 1,
+				text: 'something'
+			},
+			{
+				id: 2,
+				text: 'something else'
+			}
+		];
+
+		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+		var todoComponents = TestUtils.scryRenderedComponentsWithType(TodoList, Todo);
+
+		expect(todoComponents.length).toBe(todos.length);
 	});
 });
