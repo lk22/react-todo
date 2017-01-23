41e824d745b02f6e92defb6132f0799da719e748
Leo Knudsen <nightskater-220@hotmail.com>
2017-01-23 23:46:12 +0100

    learned the spread operator with ES6

---
+++ spread.js
@@ -0,0 +1,27 @@
+/**
+ * ES6 tutorial Spread operator
+ */
+
+var person = ['Leo', 24];
+var personTwo = ['Steven', 18];
+
+
+function greet(name, age)
+{
+	console.log('Hi ' + name + ' you are ' + age + ' years old');
+}
+
+greet(...person);
+
+var names = [
+	'Leo',
+	'Steven',
+ 	'Tom',
+ 	'Joan'
+];
+
+var final = [...names];
+
+final.forEach((name) => {
+	console.log('hi ' + name);
+});
